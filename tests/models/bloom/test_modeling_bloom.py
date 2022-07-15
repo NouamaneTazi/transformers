c0cf5d7e112138d01a5386cd9082a7dd7e3b2765
Nouamane Tazi <nouamane98@gmail.com>
2022-07-15 10:59:35 +0200

    export type to fp16

--- tests/models/bloom/test_modeling_bloom.py
+++ tests/models/bloom/test_modeling_bloom.py
@@ -396,7 +396,7 @@ class BloomModelTest(ModelTesterMixin, GenerationTesterMixin, unittest.TestCase)
         # >=760m + allow_fp16_reduced_precision_reduction = False  + torch.bmm  ==> PASS
 
         path_350m = "bigscience/bloom-350m"
-        model = BloomForCausalLM.from_pretrained(path_350m, use_cache=True).cuda()
+        model = BloomForCausalLM.from_pretrained(path_350m, use_cache=True, torch_dtype=torch.float16).cuda()
         model = model.eval()
         tokenizer = BloomTokenizerFast.from_pretrained(path_350m)
 
